d4fc216320bac84b95f7463e90d2f620f3285b5c
Sam Greasby <95031251+SamGreasby@users.noreply.github.com>
2024-11-11 22:35:40 +0000

    updated round winner counter to be more inline with TheOdinProject Exercise

--- JavaScript.js
+++ JavaScript.js
@@ -23,54 +23,55 @@ function getHumanChoice() {
     return humanChoice;
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
 /* Round Functionality */
 
 function playRound(humanChoice, computerChoice) {
-    let winner;
 
     // if player and computer pick the same choice
     if (computerChoice === humanChoice) {
         console.log("Draw! Play again!");
-        return winner = 0;
     }
 
     // rock beats scissors
     if (humanChoice === "rock" && computerChoice === "scissor") {
         console.log("You win! rock beats scissor!");
-        return winner = 1;
+        humanScore ++
     } else if (computerChoice === "rock" && humanChoice === "scissor") {
         console.log("You lose! rock beats scissor!");
-        return winner = 2;
+        computerScore ++
     }
 
     // paper beats rock
     if (humanChoice === "paper" && computerChoice === "rock") {
         console.log("You win! paper beats rock!");
-        return winner = 1;
+        humanScore ++
     } else if (computerChoice === "paper" && humanChoice === "rock") {
         console.log("You lose! paper beats rock!");
-        return winner = 2;
+        computerScore ++
     }
 
     // scissors beats paper
     if (humanChoice === "scissors" && computerChoice === "paper") {
         console.log("You win! scissors beats paper!");
-        return winner = 1;
+        humanScore ++
     } else if (computerChoice === "scissors" && humanChoice === "paper") {
         console.log("You lose! scissors beats paper!");
-        return winner = 2;
+        computerScore ++
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
+
 
 
 function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let winner;
+for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+}
 }
 
 
