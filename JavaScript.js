e200716baeb0a8500a8585a1c1892bf4bc1aaaab
Sam Greasby <95031251+SamGreasby@users.noreply.github.com>
2024-11-11 21:31:37 +0000

    updated getComputerChoice and added getHumanChoice. Basic structure for playRound is added

--- JavaScript.js
+++ JavaScript.js
@@ -2,12 +2,33 @@
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice;
     if (randomNumber === 0) {
-        return "Rock";
+        computerChoice = "rock";
     } else if (randomNumber === 1) {
-        return "Paper";
+        computerChoice = "paper";
     } else if (randomNumber === 2) {
-        return "Scissors";
+        computerChoice = "scissors";
     }
+    console.log(`Computer chooses ${computerChoice}`);
+    return computerChoice;
 }
-console.log(getComputerChoice());
+/* Human Choice Functionality */
+
+function getHumanChoice(HumanChoice) {
+    const playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+    console.log(`Player chooses ${playerChoice}`);
+    return playerChoice;
+}
+const humanScore = 0
+const computerScore = 0
+
+function playRound(humanChoice, computerChoice) {
+
+}
+
+
+const humanSelection = getComputerChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
